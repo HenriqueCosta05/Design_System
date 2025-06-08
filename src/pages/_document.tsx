608061e6a86e81602ada9acaf548348e82ffcf57
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2025-06-08 20:13:03 -0300

    feat: new component library for a project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Inter } from "next/font/google";
 
 export default function Document() {
+  const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+  });
   return (
-    <Html lang="en">
+    <Html lang="en" className={inter.variable}>
       <Head />
       <body className="antialiased">
         <Main />
